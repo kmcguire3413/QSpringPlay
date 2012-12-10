202113724d347207158a00c1ea37c9add729ecc6
kmcguire <kmcg3413@gmail.com>
2012-12-09 23:24:41 -0600

    minor changes to ChatPanel

--- src/com/kmcguire/slc/ChatPanel.java
+++ src/com/kmcguire/slc/ChatPanel.java
@@ -1,16 +1,30 @@
 package com.kmcguire.slc;
 
+import com.trolltech.qt.gui.QResizeEvent;
+import com.trolltech.qt.gui.QWidget;
+
 public class ChatPanel extends Panel {
-    private String                  channel;
-    private MainWindow              mwin;
+    public String                  channel;
+    public MainWindow              mwin;
+    public QWidget                 surface;
     
     public ChatPanel(MainWindow mwin, String channel) {
         this.mwin = mwin;
         this.channel = channel;
+        
+        surface = new QWidget(this);
+        surface.setStyleSheet("background-color: #ffff33;");
+        surface.show();
     }
     
     @Override
     public String getTitle() {
         return String.format("#%s", channel);
+    }
+    
+    
+    @Override
+    public void resizeEvent(QResizeEvent event) {
+        surface.resize(width(), height());
     }
 }
