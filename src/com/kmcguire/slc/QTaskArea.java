92a470b1f3306f6d1045031c14ee231e5f5bb417
kmcguire <kmcg3413@gmail.com>
2012-12-09 18:50:35 -0600

    ADDED SCROLLBAR IMPLEMENTATION FOR QTASKAREA + added scrollbar implementation for QTaskArea

--- src/com/kmcguire/slc/QTaskArea.java
+++ src/com/kmcguire/slc/QTaskArea.java
@@ -17,20 +17,49 @@ public class QTaskArea extends QWidget {
         widgets = new ArrayList<QWidget>();
         surface = new QWidget(this);
         scrollbar = new QScrollBar(this);
+        
+        scrollbar.valueChanged.connect(this, "scrollbarChanged(int)");
+        
         resizeEvent(width(), height());
         
         surface.setStyleSheet("background-color: #ff9999;");
     }
     
+    public void scrollbarChanged(int value) {
+        int     y;
+        
+        y = -value;
+        for (QWidget widget : widgets) {
+            widget.move(0, y);
+            y += widget.height();
+        }
+    }
+    
     public void addWidget(QWidget widget) {
+        widget.setParent(this);
         widgets.add(widget);
     }
     
     private void resizeEvent(int w, int h) {
+        int     th;
+        
         surface.move(0, 0);
         surface.resize(w - 20, h);
         scrollbar.move(w - 20, 0);
         scrollbar.resize(20, h);
+        
+        th = 0;
+        for (QWidget widget : widgets) {
+            th += widget.height();
+        }
+        
+        if (surface.height() >= th) {
+            scrollbar.setMaximum(0);
+        } else {
+            scrollbar.setMaximum(th - surface.height());
+        }
+        
+        scrollbarChanged(scrollbar.value());
     }
     
     @Override
